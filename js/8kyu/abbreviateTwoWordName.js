22e6a699d2a47d42d7c7494ee275b457bad2b508
agildanugrah <agildanugrah@gmail.com>
2023-12-09 16:41:54 +0700

    Convert name to initials

---
+++ js/8kyu/abbreviateTwoWordName.js
@@ -0,0 +1,12 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+// My solution
+const abbrevName = name => name.split(' ').map(initials => initials.charAt(0).toUpperCase()).join('.')
