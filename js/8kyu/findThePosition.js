ddb883a29876341bdd59ade8f530e75987724575
agildanugrah <agildanugrah@gmail.com>
2024-01-22 23:07:01 +0700

    Return the position of the alphabet

---
+++ js/8kyu/findThePosition.js
@@ -0,0 +1,12 @@
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+// My solution
+function position(letter){
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    const position = alphabet.indexOf(letter.toLowerCase()) + 1
+    return `Position of alphabet: ${position}`;
+}
