d5b6e9cc5497a8866678d660c7268aac8ac57e68
agildanugrah <agildanugrah@gmail.com>
2024-01-25 21:56:55 +0700

    Store "codewa.rs" as a variable called name

---
+++ js/8kyu/basicVariable.js
@@ -0,0 +1,7 @@
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+// My solution
+// Change double equal sign "==" to an equal sign "="
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
